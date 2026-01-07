7237d0474f314cb4cccb9191e5c9545af3890a8f
enklht <enklht@gmail.com>
2026-01-07 23:10:13 +0900

    fix: :bug: correct the acceleration sign along the x axis

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn get_acceleration() -> Vec2 {
     let acc = {
         #[cfg(target_arch = "wasm32")]
         unsafe {
-            vec2(get_accel_x() as f32, get_accel_y() as f32)
+            vec2(-get_accel_x() as f32, get_accel_y() as f32)
         }
 
         #[cfg(not(target_arch = "wasm32"))]
